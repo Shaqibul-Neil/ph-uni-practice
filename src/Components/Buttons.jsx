3d2ba2e376b1cad245a14d5ce5fdcde479c70464
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-24 15:56:45 +0600

    toggle btn

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,10 +1,24 @@
 import Container from "./Container";
 
 const Buttons = () => {
+  const btns = ["All", "Pending", "Submitted", "Reviewed"];
   return (
     <>
       <Container>
-        <div></div>
+        <div className="text-right mb-16">
+          {btns.map((btn, i) => (
+            <button
+              className={`${
+                i === 0 ? "toggle-btn rounded-l-md" : "toggle-btn"
+              } ${
+                i === btns.length - 1 ? "toggle-btn rounded-r-md" : "toggle-btn"
+              }`}
+              key={i}
+            >
+              {btn}
+            </button>
+          ))}
+        </div>
       </Container>
     </>
   );
